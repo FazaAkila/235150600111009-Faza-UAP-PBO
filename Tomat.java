36e32b066929e40d5e6e60d8f42f759dddf539e6
FazaAkila <79125027+FazaAkila@users.noreply.github.com>
2024-05-31 15:04:17 +0700

    Create Tomat.java

---
+++ Tomat.java
@@ -0,0 +1,10 @@
+class Tomat extends Tanaman implements Perawatan {
+    public Tomat() {
+        setMasaHidup(100);
+        setBerbuah(100);
+        setPerkembangan(25);
+        setProsesBerbuah(0);
+        setLamaHidup(0);
+        setBuah(0);
+    }
+}

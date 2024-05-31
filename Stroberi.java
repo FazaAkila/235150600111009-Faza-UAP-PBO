0aaddbb3b0e49840dbfc48ba7303782835ef605b
FazaAkila <79125027+FazaAkila@users.noreply.github.com>
2024-05-31 15:00:51 +0700

    Create Stroberi.java

---
+++ Stroberi.java
@@ -0,0 +1,10 @@
+class Stroberi extends Tanaman implements Perawatan {
+    public Stroberi() {
+        setMasaHidup(60);
+        setBerbuah(150);
+        setPerkembangan(35);
+        setProsesBerbuah(0);
+        setLamaHidup(0);
+        setBuah(0);
+    }
+}

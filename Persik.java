5c679664de59754c2f666297aca36358c6825f3e
FazaAkila <79125027+FazaAkila@users.noreply.github.com>
2024-05-31 15:02:44 +0700

    Create Persik.java

---
+++ Persik.java
@@ -0,0 +1,10 @@
+class Persik extends Tanaman implements Perawatan {
+    public Persik() {
+        setMasaHidup(180);
+        setBerbuah(250);
+        setPerkembangan(15);
+        setProsesBerbuah(0);
+        setLamaHidup(0);
+        setBuah(0);
+    }
+}
